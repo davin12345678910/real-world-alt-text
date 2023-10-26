10f507c4c9cf69f6b8a2467db57060a6ae3e8e4f
davin12345678910 <davinkyi@gmail.com>
2023-10-26 16:27:33 -0700

    "adding in new template methods"

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ import blip2
 import requests
 from PIL import Image
 
+
+'''
+Description: this method will allow us to recieve json information from RTMDet
+'''
+def run_oneFormer(img):
+    print("running oneFormer")
+
+def run_rtmdet(img):
+    print("running rtmdet")
+
+# LLaVA works now!
+def run_llava(img):
+    print("running llava")
+
+
+
 def get_description():
     # Use a breakpoint in the code line below to debug your script.
 
